d7edd94bc234977783801237db40f66362ff1399
tomas010101 <84735708+tomas010101@users.noreply.github.com>
2021-10-31 16:07:49 -0300

    Imagenes1
    Se viene feo el asunto

---
+++ Foromanager/Foromanager/Models/Imagenes.cs
@@ -0,0 +1,9 @@
+﻿namespace Foromanager.Models
+{
+    public class Imagenes
+    {
+        public int ImagenId { get; set; }
+        public string ImagenNombre { get; set; }
+      //  public byte[8388608] Imagen { get; set; } //todos trolos
+    }
+}
