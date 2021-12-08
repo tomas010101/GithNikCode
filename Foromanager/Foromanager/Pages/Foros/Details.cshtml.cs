387cb6e748d0cd8d80d826e5ebe8c150409416ab
Bruno-powershell <brunobarrionuevo2003@gmail.com>
2021-12-07 23:09:12 -0300

    Cambios de oridios

--- Foromanager/Foromanager/Pages/Foros/Details.cshtml.cs
+++ Foromanager/Foromanager/Pages/Foros/Details.cshtml.cs
@@ -15,127 +15,127 @@ using System.IO;
 
 namespace Foromanager.Pages.Foros
 {
-    public enum Acciones
-    {
-        postear,
-        like,
-        dislike,
-        aprobar,
-        descartar
-    }
+	public enum Acciones
+	{
+		postear,
+		like,
+		dislike,
+		aprobar,
+		descartar
+	}
 
-    public class DetailsModel : DI_BasePageModel
-    {
-        private readonly Foromanager.Data.ApplicationDbContext _context;
-        
-        public DetailsModel(Foromanager.Data.ApplicationDbContext context,IAuthorizationService authorizationService,UserManager<Usuario> userManager): base(context,authorizationService,userManager)
-        {
-            _context = context;
-        }
-        [BindProperty]
-        public Publicacion Publicacion { get; set; }
+	public class DetailsModel : DI_BasePageModel
+	{
+		private readonly Foromanager.Data.ApplicationDbContext _context;
 
-        public Foro Foro { get; set; }
+		public DetailsModel(Foromanager.Data.ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<Usuario> userManager) : base(context, authorizationService, userManager)
+		{
+			_context = context;
+		}
+		[BindProperty]
+		public Publicacion Publicacion { get; set; }
 
-        public Acciones acciones {get;set;}
-        
-        public int PublicacionId {get;set;}
+		public Foro Foro { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            Foro = await _context.Foro
-                        .Include(s=>s.Publicaciones)
-                        .AsNoTracking()
-                        .FirstOrDefaultAsync(m=>m.ForoId == id);
-            
-            if (id == null || Foro == null)
-            {
-                return NotFound();
-            }
-            
-            var isAuthorizated = User.IsInRole(Constants.ForumManagersRole) || User.IsInRole(Constants.ForumAdministratorsRole);
-            var currentUserId = UserManager.GetUserId(User);
-            
-            if(!isAuthorizated && currentUserId!=Foro.OwnerID && Foro.Status != ForumStatus.Aprobado)
-            {
-                return Forbid();
-            }
-            return Page();
-        }
+		[BindProperty]
+		public Acciones acciones { get; set; }
 
-        [BindProperty]
-        public IFormFile ImgCarga { get; set; }
+		public int PublicacionId { get; set; }
 
-        public async Task<IActionResult> OnPostAsync(int id)
-        {
-            var archivo = HttpContext.Request.Form.Files.FirstOrDefault();
-            Imagenes imagen = null;
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			Foro = await _context.Foro
+						.Include(s => s.Publicaciones)
+						.AsNoTracking()
+						.FirstOrDefaultAsync(m => m.ForoId == id);
 
-            if (archivo != null)
-            {
-                imagen = new Imagenes();
-                using (var bReader = new BinaryReader(archivo.OpenReadStream()))
-                {
-                    imagen.Imagen = bReader.ReadBytes((int)archivo.Length);
-                    imagen.ImagenNombre = archivo.Name;
-                }
-            }
+			if (id == null || Foro == null)
+			{
+				return NotFound();
+			}
 
-            Foro = await _context.Foro.FirstOrDefaultAsync(m=>m.ForoId==id);
+			var isAuthorizated = User.IsInRole(Constants.ForumManagersRole) || User.IsInRole(Constants.ForumAdministratorsRole);
+			var currentUserId = UserManager.GetUserId(User);
 
-            if(Foro == null)
-            {
-                return NotFound();
-            }
+			if (!isAuthorizated && currentUserId != Foro.OwnerID && Foro.Status != ForumStatus.Aprobado)
+			{
+				return Forbid();
+			}
+			return Page();
+		}
 
-            var forumOperation = (Foro.Status == ForumStatus.Aprobado)? ForumOperations.Approve : ForumOperations.Reject;
-            var isAuthorizated = await AuthorizationService.AuthorizeAsync(User,Foro,forumOperation);
+		[BindProperty]
+		public IFormFile ImgCarga { get; set; }
 
-            if (!isAuthorizated.Succeeded && UserManager.GetUserId(User)!=Foro.OwnerID && Foro.Status != ForumStatus.Aprobado)
-            {
-                return Forbid();
-            }
+		public async Task<IActionResult> OnPostAsync(int id)
+		{
+			var archivo = HttpContext.Request.Form.Files.FirstOrDefault();
+			Imagenes imagen = null;
 
-            switch(acciones)
-            {
-                case Acciones.like:
-                    Publicacion = await _context.Publicacion.Include(r=>r.Reacciones).FirstOrDefaultAsync(p=>p.PublicacionId == PublicacionId); 
-                    Publicacion = await _context.Publicacion.AsNoTracking().Include(r =>r.Reacciones).FirstOrDefaultAsync(p =>p.PublicacionId == PublicacionId); 
-                    if(Publicacion.Reacciones.Count < 0)
-                    {
-                        Publicacion.Reacciones = new List<Reaccion>();
-                        Publicacion.Reacciones.Add(new Reaccion(){Like=true, Usuario = User.Identity.Name});   
-                    }
-                    _context.Attach(Publicacion).State = EntityState.Modified;
-                    break;
-                case Acciones.dislike:
-                    Publicacion = await _context.Publicacion.Include(r =>r.Reacciones).FirstOrDefaultAsync(p=>p.PublicacionId==PublicacionId);
-                    if(Publicacion.Reacciones.Count < 0)
-                    {
-                        Publicacion.Reacciones = new List<Reaccion>();
-                        Publicacion.Reacciones.Add(new Reaccion(){DisLike=true, Usuario = User.Identity.Name});   
-                                      
-                    }
-                    _context.Attach(Publicacion).State = EntityState.Modified;
-                    break;
-                case Acciones.aprobar:
-                    Foro.Status = ForumStatus.Aprobado;
-                    break;
-                case Acciones.descartar:
-                    Foro.Status = ForumStatus.Rechazado;
-                    break;
-                case Acciones.postear:
-                    Publicacion.ForoId = id;
-                    Publicacion.Usuario = User.Identity.Name.Split('-')[0]+" "+ User.Identity.Name.Split('-')[1];
-                    Publicacion.Imagen = imagen;
-                    Publicacion.Fecha = DateTime.Now;
-                    _context.Publicacion.Add(Publicacion);
-                    break;
-            }
+			if (archivo != null)
+			{
+				imagen = new Imagenes();
+				using (var bReader = new BinaryReader(archivo.OpenReadStream()))
+				{
+					imagen.Imagen = bReader.ReadBytes((int)archivo.Length);
+					imagen.ImagenNombre = archivo.Name;
+				}
+			}
 
-            await _context.SaveChangesAsync();
+			Foro = await _context.Foro.FirstOrDefaultAsync(m => m.ForoId == id);
 
-            return RedirectToPage("Details", new { id = id });
-        }
-    }
+			if (Foro == null)
+			{
+				return NotFound();
+			}
+
+			var forumOperation = (Foro.Status == ForumStatus.Aprobado) ? ForumOperations.Approve : ForumOperations.Reject;
+			var isAuthorizated = await AuthorizationService.AuthorizeAsync(User, Foro, forumOperation);
+
+			if (!isAuthorizated.Succeeded && UserManager.GetUserId(User) != Foro.OwnerID && Foro.Status != ForumStatus.Aprobado)
+			{
+				return Forbid();
+			}
+
+			switch (acciones)
+			{
+				case Acciones.like:
+					//Publicacion = await _context.Publicacion.Include(r => r.Reacciones).FirstOrDefaultAsync(p => p.PublicacionId == PublicacionId);
+					Publicacion = await _context.Publicacion.AsNoTracking().Include(r => r.Reacciones).FirstOrDefaultAsync(p => p.PublicacionId == PublicacionId);
+					if (!Publicacion.Reacciones.Any())
+					{
+						Publicacion.Reacciones = new List<Reaccion>();						
+					}
+					Publicacion.Reacciones.Add(new Reaccion() { Like = true, Usuario = User.Identity.Name });
+					_context.Attach(Publicacion).State = EntityState.Modified;
+					break;
+				case Acciones.dislike:
+					Publicacion = await _context.Publicacion.AsNoTracking().Include(r => r.Reacciones).FirstOrDefaultAsync(p => p.PublicacionId == PublicacionId);
+					if (!Publicacion.Reacciones.Any())
+					{
+						Publicacion.Reacciones = new List<Reaccion>();						
+					}
+					Publicacion.Reacciones.Add(new Reaccion() { DisLike = true, Usuario = User.Identity.Name });
+					_context.Attach(Publicacion).State = EntityState.Modified;
+					break;
+				case Acciones.aprobar:
+					Foro.Status = ForumStatus.Aprobado;
+					break;
+				case Acciones.descartar:
+					Foro.Status = ForumStatus.Rechazado;
+					break;
+				case Acciones.postear:
+					Publicacion.ForoId = id;
+					Publicacion.Usuario = User.Identity.Name.Split('-')[0] + " " + User.Identity.Name.Split('-')[1];
+					Publicacion.Imagen = imagen;
+					Publicacion.Fecha = DateTime.Now;
+					_context.Publicacion.Add(Publicacion);
+					break;
+			}
+
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("Details", new { id = id });
+		}
+	}
 }
